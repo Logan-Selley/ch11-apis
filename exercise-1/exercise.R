f84d1340c7fe0e3910c28e5cc134623be10d3165
Logan-Selley <archane24@gmail.com>
2018-04-20 17:46:49 -0700

    exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,14 +7,14 @@ library(jsonlite)
 library(dplyr)
 
 # Create a variable for the API's base URI (https://api.github.com)
-
+api <- "https://api.github.com"
 
 # Under the "Repositories" category of the API, 
 # find the endpoint that will list repos in an organization. Then, 
 # Create a variable `resource` that stores the endpoint for the "info201"
 # organization repos (this is the PATH to the resource of interest).
 # (FYI: this is where we keep the book code and master exercise sets!)
-
+GET("https://api.github.com/users/info201b-s18/")
 
 # Send a GET request to this endpoint (the `base_uri`` followed by `resource`)
 
